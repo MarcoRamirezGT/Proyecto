9d66ede1eaff5aceab5af7717f4e58d50b6c28eb
MarcoRamirezGT <46507192+MarcoRamirezGT@users.noreply.github.com>
2020-02-27 21:39:41 -0600

    Initial commit

---
+++ Proyecto/Assets/Scripts/MO.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+
+public class MO : MonoBehaviour
+{
+    public Transform target;
+    public float speed;
+    private Rigidbody playerRb;
+    public float force;
+    public Text back;
+    public Text front;
+
+   // public CharacterController playerCon;
+
+   
+   
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = this.GetComponent<Rigidbody>();
+       // playerCon = this.GetComponent<CharacterController>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+          
+                Jump();
+           
+            
+
+        }
+    }
+    private void Jump()
+    {
+        if (playerRb && Mathf.Abs(playerRb.velocity.y) < 0.05f)
+            playerRb.AddForce(0, force, 0, ForceMode.Impulse);
+         //transform.Rotate(new Vector3(0f, 0.0f, -90f)*Time.fixedTime*2);
+           // playerRb.transform.Rotate(180 * Vector3.forward, Space.World);
+       
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Trampa"))
+        {
+            SceneManager.LoadScene("Lvl1");
+        }
+        /*  if (other.gameObject.CompareTag("Velocidad7.5"))
+          {
+              speed = 7.5;
+          }*/
+        if (other.gameObject.CompareTag("Velocidad8"))
+        {
+            speed = 8;
+
+        }
+        if (other.gameObject.CompareTag("Velocidad13"))
+        {
+            speed = 13;
+
+        }
+        if (other.gameObject.CompareTag("Velocidad15"))
+        {
+            speed = 15;
+        }
+        if (other.gameObject.CompareTag("Velocidad30"))
+        {
+            speed = 30;
+        }
+        if (other.gameObject.CompareTag("Finish"))
+        {
+            front.text = "WIN!";
+            back.text = "WIN!";
+
+        }
+
+    }
+
+}
+
---
+++ Proyecto/Assets/Scripts/Movement.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Vector3 playerInput;
+    public CharacterController player;
+    public float playerSpeed;
+    public float gravity;
+    public float fallVelocity;
+
+    public Camera mainCamera;
+    private Vector3 camForward;
+    private Vector3 camRight;
+    private Vector3 movePlayer;
+    public float horizontalMove;
+    public float verticalMove;
+    public float jumpForce;
+    public Transform target;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float step = playerSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+        // horizontalMove = Input.GetAxis("Horizontal");
+        // verticalMove = Input.GetAxis("Vertical");
+
+        //  playerInput = new Vector3(horizontalMove, 0, verticalMove);
+        // playerInput = Vector3.ClampMagnitude(playerInput, 1);
+
+       CamDirection();
+
+        movePlayer = playerInput.x * camRight + playerInput.z * camForward;
+        movePlayer = movePlayer * playerSpeed;
+        player.transform.LookAt(player.transform.position + movePlayer);
+       
+
+        SetGravity();
+
+        PlayerSkills();
+
+
+       player.Move(movePlayer * Time.deltaTime);
+      //  Debug.Log(player.isGrounded);
+    }
+    public void PlayerSkills()
+    {
+        if (player.isGrounded && Input.GetButtonDown("Jump"))
+        {
+            fallVelocity = jumpForce;
+            movePlayer.y = fallVelocity;
+        }
+    }
+    public void CamDirection()
+    {
+        camForward = mainCamera.transform.forward;
+        camRight = mainCamera.transform.right;
+
+        camForward.y = 0;
+        camRight.y = 0;
+
+        camForward = camForward.normalized;
+        camRight = camRight.normalized;
+
+    }
+    public void SetGravity()
+    {
+        if (player.isGrounded)
+        {
+            fallVelocity = -gravity * Time.deltaTime;
+            movePlayer.y = fallVelocity;
+
+        }
+        else
+        {
+            fallVelocity -= gravity * Time.deltaTime;
+            movePlayer.y = fallVelocity;
+
+        }
+    }
+}
